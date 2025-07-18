8c6599a011e9d460aa020510a4d7379de2842848
paulohenriqueo <paulohenrique300305@gmail.com>
2025-07-18 20:51:52 -0300

    :sparkles: feat: closes #3 - Tratamento de Erros

--- js/script.js
+++ js/script.js
@@ -4,18 +4,36 @@ function search(){
 
     var url = `https://api.github.com/users/${username}`;
 
-    $.getJSON(url, (user) => {
-        
-
-        document.getElementById("name").innerHTML       = user.name;
-        document.getElementById("html_url").innerHTML   = user.html_url;
-        document.getElementById("company").innerHTML    = user.company;
-
-        document.getElementById("avatar_url").innerHTML = 
-        `
-            <img src="${user.avatar_url}" width="220"
-                height="220" class="shadow rounded"></img>
-        `;
-                        
+    $.getJSON(url, (user) => {    
+        showUserData(user);
+        clearError();                        
+    }).fail( () => {
+        showUserData({});
+        showError("Não Encontrado!");        
     });
+}
+
+function showError(msg){
+    document.getElementById("error").innerHTML = 
+        `
+        <div class="alert alert-danger mt-1" role="alert">
+            ${msg}
+        </div>
+        `;
+}
+
+function clearError(){
+    document.getElementById("error").innerHTML = "";
+}
+
+function showUserData(user){
+    document.getElementById("name").innerHTML       = user.name     || "";
+    document.getElementById("html_url").innerHTML   = user.html_url || "";
+    document.getElementById("company").innerHTML    = user.company  || "";
+
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ?
+        `
+            <img src="${user.avatar_url}" width="220" height="220" class="shadow rounded"></img>
+        ` : 
+        "";
 }
